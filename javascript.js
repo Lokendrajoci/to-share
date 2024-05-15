24b153779588acd3014b3dbf698c48fd2739608d
Lokendra Joshi <100509803+Lokendrajoci@users.noreply.github.com>
2024-05-15 21:36:02 +0545

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+const namepi = document.querySelector(".tickButoon")
+namepi.addEventListener('click', () => {
+    const textvalue = document.querySelector(".toAdd")
+    const elementAdd = document.createElement("li")
+    elementAdd.className = 'poetsen-one-regular';
+    elementAdd.style.listStyleType = "none";
+
+    elementAdd.innerText = textvalue.value
+    var liItem=document.querySelector(".addUl")    
+    liItem.appendChild(elementAdd)
+    textvalue.value=''
+    
+}
+)
+
+
+
+
