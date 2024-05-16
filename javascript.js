d1e906a9e3eafb7e0c4a25e7d14437fc9e554075
Lokendra Joshi <100509803+Lokendrajoci@users.noreply.github.com>
2024-05-16 20:33:47 +0545

    javascript.js

--- javascript.js
+++ javascript.js
@@ -5,16 +5,18 @@ namepi.addEventListener('click', () => {
     
     const textvalue = document.querySelector(".toAdd")
     const elementAdd = document.createElement("li")
-    elementAdd.className = 'poetsen-one-regular';
+    // elementAdd.className = 'poetsen-one-regular';
+    elementAdd.classList.add('poetsen-one-regular');
+    elementAdd.classList.add('bakchodiNahi');
     elementAdd.style.listStyleType = "none";
 
-    elementAdd.innerText = textvalue.value
-    var liItem=document.querySelector(".addUl")    
+    elementAdd.innerHTML = '&#8226; ' + textvalue.value + '<span class="crossButton">&#10006;</span>';
+
+    var liItem=document.querySelector(".addUl") 
+  
     liItem.appendChild(elementAdd)
     textvalue.value=''
-
-
    
-    
 }
 )
+
